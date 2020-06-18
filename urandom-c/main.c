5b8e96ca645f72147961c124f15c43f56eb2d289
David Egan <david@carawebs.com>
2020-06-18 16:08:37 +0100

    Abstract out print/string utility functions

--- urandom-c/main.c
+++ urandom-c/main.c
@@ -5,8 +5,7 @@
 #include <inttypes.h>
 #include "../lib/r64/base64.h"
 #include "../lib/integer-input.h"
-
-void printInts(uint8_t *bytes, int n);
+#include "../lib/utilities.h"
 
 void setRandomBytesUrandom(uint8_t **bytesBuffer, size_t n)
 {
@@ -30,38 +29,24 @@ int main()
 	
 	// `pureBytes` is now a pointer to `numBytes` uint8_ts that have been
 	// randomly drawn from /dev/urandom. Some of these bytes may not be representable
-	// by printable ASCII characters. To print them to stdout, we can cast them to ints:
-//	for (int i = 0; i < numBytes; i++) {
-//		printf("%d\n", (int)pureBytes[i]);
-//	}
-//	printf("\n");
+	// by printable ASCII characters. 
 	printInts(pureBytes, numBytes);
+	printHex(pureBytes, numBytes);
 
-	// You could also print bytes to stdout as hex characters:
-	printf("pureBytes printed in hexadecimal format\n");
-	for (int i = 0; i < numBytes; ++i) {
-		printf("%02X", pureBytes[i]);
-	}
-	printf("\n");
-	
 	// If printable characters are needed (e.g. for a password) converting the uint8_ts
 	// to a string of chars that are base 64 encoded is a good option:
 	uint8_t *b64Buffer = malloc(lenCharsBase64(numBytes));
 	base64Encode(pureBytes, b64Buffer, numBytes);
 	printf("pureBytes as a radix 64 string:\n%s\n", b64Buffer);
-	
+
+	// Get a hexstring
+	char *hexstring = calloc((numBytes * 2) + 1, sizeof(*hexstring)); 
+	charsToHexString(hexstring, pureBytes, (size_t)numBytes);
+	printf("Output a hexstring\n");
+	printf("%s\n", hexstring);
+	free(hexstring);
 	free(pureBytes);
 	free(b64Buffer);
 
 	return 0;
-}
-
-void printInts(uint8_t *bytes, int n)
-{
-	printf("Bytes printed as ints.\n");
-	printf("[ ");
-	for (int i = 0; i < n; ++i) {
-		printf(i > 0 ? ", %d" : "%d", (int)*(bytes + i));
-	}
-	printf(" ]\n");
 }
